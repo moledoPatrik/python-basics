e42f29ebd5f0478b3bbcdd7eab3f9239794a5449
Patrik Moledo <moledop55@gmail.com>
2023-01-12 19:50:16 +0900

    string manipulation exercise added

---
+++ exercises/03-string-manipulation/app.py
@@ -0,0 +1,16 @@
+phrase = "I am a string"
+#         0123456789012
+
+print(phrase.lower())
+print(phrase.upper())
+
+print(phrase.islower())
+print(phrase.isupper())
+
+print(len(phrase))
+
+print(phrase[0])
+print(phrase[7:])
+print(phrase[2:4])
+print(phrase.index('string'))
+print(phrase.replace('string', 'potato'))
