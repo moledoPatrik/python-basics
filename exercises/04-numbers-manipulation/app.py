b3885c062c4fe213f7202de32f31f446c2a68c9f
Patrik Moledo <moledop55@gmail.com>
2023-01-12 20:18:03 +0900

    num manipulation, input and basic calculator added

---
+++ exercises/04-numbers-manipulation/app.py
@@ -0,0 +1,22 @@
+from math import *
+
+#  +  -  *  /  **  %
+# we can use () to change order of operations like in other langs
+
+my_num = 5
+negative_num = -6
+
+print("My favorite number is " + str(my_num))
+print(abs(negative_num))
+print(pow(3, 2))
+
+print(max(1, 10))
+print(min(1, 10))
+
+print(round(3.2))
+print(round(3.7))
+
+# from math module
+print(floor(8.7))
+print(ceil(8.2))
+print(sqrt(36))
---
+++ exercises/06-basic-calculator/app.py
@@ -0,0 +1,5 @@
+num1 = input("Type your first number: ")
+num2 = input("Type your second number: ")
+
+result = float(num1) + float(num2)
+print(num1 + " + " + num2 + " is qual to " + str(result))
